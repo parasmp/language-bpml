05513313bea7cd972f4959cd0b6c7cf08f677c94
Paras Patel <paras@kyln>
2018-03-18 10:08:29 -0400

    Initial Commit: bpml language package for Atom

---
+++ spec/language-bpml-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LanguageBpmlView from '../lib/language-bpml-view';
+
+describe('LanguageBpmlView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
